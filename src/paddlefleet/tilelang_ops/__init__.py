189a7ed45194cc5790c0df5e4c6d5b8dae89bd49
yjmstr <zhangyj12024@shanghaitech.edu.cn>
2026-05-27 16:41:11 +0800

    [DeepSeekV4] Add nondeterministic TileLang CSA indexer

---
+++ src/paddlefleet/tilelang_ops/__init__.py
@@ -0,0 +1,27 @@
+import paddle
+
+paddle.enable_compat(scope={"tilelang"}, silent=True)
+
+__all__ = [
+    "tilelang_csa_compressed_indexer_bwd_paddle",
+    "tilelang_csa_compressed_indexer_topk_paddle",
+]
+
+
+def __getattr__(name):
+    if name in {
+        "tilelang_csa_compressed_indexer_bwd_paddle",
+        "tilelang_csa_compressed_indexer_topk_paddle",
+    }:
+        from .csa_indexer_core import (
+            tilelang_csa_compressed_indexer_bwd_paddle,
+            tilelang_csa_compressed_indexer_topk_paddle,
+        )
+
+        exports = {
+            "tilelang_csa_compressed_indexer_bwd_paddle": tilelang_csa_compressed_indexer_bwd_paddle,
+            "tilelang_csa_compressed_indexer_topk_paddle": tilelang_csa_compressed_indexer_topk_paddle,
+        }
+        globals().update(exports)
+        return exports[name]
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
